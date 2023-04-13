d3506cf358867f858b5ffa2d9ec8da7bab368366
kanwarmamta <94844123+kanwarmamta@users.noreply.github.com>
2023-04-13 20:52:05 +0530

    Update student.php

--- student.php
+++ student.php
@@ -1,16 +1,19 @@
 <!DOCTYPE html>
     <head lang="en">
-        <title>Student_Login</title>
+        <title>Student Login</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
     </head>
     <body>
         <h1>Student Login Page</h1>  
         <form action="student.php" method="get">
-           Roll No: <input type="char" name="sRollNo" maxlength="8"/><br>
+           Roll No: <input type="char" name="stRollno" maxlength="8"/><br>
            <!-- Name: <input type="varchar" name="sName" maxlength="50"/><br>
            Webmail: <input type="varchar" name="sWebmail" maxlength="100"/><br> -->
             Password: <input type="password" name="stPassword" maxlength="20"/><br>
-            <input type="submit" name="st_login" value="Login"/>
+            <input type="submit" name="login" value="Login"/>
+            <p>
+  		Not a member? <a href="st_register.php">Register</a>
+  	</p>
         </form>
     </body>
 </html>
@@ -19,6 +22,9 @@
 session_start();
 error_reporting(0);
 require_once 'dbconfig.php';
+if(!empty($_GET['status'])){
+    echo '<div>You have been logged out!</div>';
+}
 
 $err = "";
 $result=true;
@@ -30,35 +36,37 @@ if ($result)
         echo $err;
     }
     else
-    {
-        $RollNo=$_GET["stRollno"];
+    {   
+       
+        $stRollno=$_GET["stRollno"];
         $pwd=$_GET["stPassword"];
         if(empty($err))
         {
-            $sql = "SELECT stRollno, stName, stWebmail, stPhone, password FROM student WHERE stRollno=?";
+            $sql = "SELECT stRollno, stName, stWebmail, stPhone, stPassword FROM student WHERE stRollno=?";
             $stmt = mysqli_prepare($conn, $sql);
-            mysqli_stmt_bind_param($stmt, "s", $RollNo);
-            $param_username=$RollNo;
+            mysqli_stmt_bind_param($stmt, "s", $stRollno);
+            $param_username=$stRollno;
     
             if(mysqli_stmt_execute($stmt))
             {
                 mysqli_stmt_store_result($stmt);
                 if(mysqli_stmt_num_rows($stmt)==1)
                 {
-                    mysqli_stmt_bind_result($stmt, $stRollno, $stName, $stWebmail, $stPhone, $hashed_password);
+                    mysqli_stmt_bind_result($stmt, $stRollno, $stName, $stWebmail, $stPhone, $stPassword);
                     if(mysqli_stmt_fetch($stmt))
                     {
-                        if($pwd==$hashed_password)
+                       
+                        if($pwd==$stPassword)
                         {
                             // this means the password is corrct. Allow user to login
                             session_start();
-                            $_SESSION["stRollno"] = $RollNo;
-                            $_SESSION["stName"] = $Name;
-                            $_SESSION["stWebmail"] = $Webmail;
-                            $_SESSION["stPhone"] = $Phone;
+                            $_SESSION["stRollno"] = $stRollno;
+                            $_SESSION["stName"] = $stName;
+                            $_SESSION["stWebmail"] = $stWebmail;
+                            $_SESSION["stPhone"] = $stPhone;
                             $_SESSION["loggedin"] = true;
                             //Redirect user to welcome page
-                            header("location: dbwelcome.php");
+                            header("location: st_view.php");
                         }
                         else
                         {
