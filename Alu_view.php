8b6628ed304de884bfef1d6c1f7e15f694d810a2
Amisha-Raje <101448515+Amisha-Raje@users.noreply.github.com>
2023-04-13 17:49:59 +0530

    Add files via upload

--- Alu_view.php
+++ Alu_view.php
@@ -48,7 +48,7 @@ if(!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !==true)
 
         <hr>
 
-        <input type="button" value="Update Info" class="homebutton" id="u" onClick="document.location.href='Alu_update.php'" />
+        <input type="button" value="Edit Profile" class="homebutton" id="u" onClick="document.location.href='Alu_update.php'" />
         <input type="button" value="Delete user" class="homebutton" id="d" onClick="document.location.href='Alu_delete.php'" />
 
       <input type="button" value="Logout" class="homebutton" id="l" onClick="document.location.href='Alu_logout.php'" />
