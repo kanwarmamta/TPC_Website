4fd41697b4a9e0e4dc84cc96b1b41379739acd48
NishitaLath <95167563+NishitaLath@users.noreply.github.com>
2023-06-22 17:01:33 +0530

    Update dbconfig.php

--- dbconfig.php
+++ dbconfig.php
@@ -1,7 +1,7 @@
 <?php
     define('DB_SERVER', 'localhost');
     define('DB_USERNAME', 'root');
-    define('DB_PASSWORD', 'Nkl@2323');
+    define('DB_PASSWORD', '********');
     define('DB_NAME', 'dbpro');
 
     //Try connecting to the Database
